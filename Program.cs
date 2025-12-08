8f88a98dc482d9d82a2adfc18dc3ed8f56776006
KennaCo14 <kenna.co14@gmail.com>
2025-12-08 10:59:13 -0700

    made a greeting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Mykenna
+// 12/8/25
+//Lab 7
+
+Console.WriteLine("Lets make a phrase into pig latin and encrypt it!");
+Console.WriteLine("Give me a phrase to turn into pig latin:");
+Console.Write("Phrase: ");
+string[] input = Console.ReadLine().ToLower().Split(" ");
+
+Console.WriteLine(input);
